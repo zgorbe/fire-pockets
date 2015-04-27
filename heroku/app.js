71d8f69d7a3ba01fa9c95323a4dca3b095e7729c
Zoltan Gorbe <zoltan_gorbe@epam.com>
2015-04-27 12:49:49 +0200

    Express config to handle js/css files

--- heroku/app.js
+++ heroku/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
  
+app.use(express.static(__dirname));
+
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
 }).listen(port);
